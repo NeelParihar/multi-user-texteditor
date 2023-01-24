344a5aa36327f68a3fa0d72221ebb3401054385b
NeelParihar <neelparihar599@gmail.com>
2023-01-24 20:26:48 +0530

    intialized client application
    Signed-off-by: NeelParihar <neelparihar599@gmail.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import TextEditor from './components/TextEditor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TextEditor/>
+    </div>
+  );
+}
+
+export default App;
